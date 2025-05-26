415f9d897ab3c6fa6744b73dfcd3b1bd3ebd06a3
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-05-26 19:23:38 +0530

    Refactor Service component layout for improved styling and consistency

--- src/components/cards/Service.tsx
+++ src/components/cards/Service.tsx
@@ -10,18 +10,16 @@ export const Service = ({ title, description, icon }: ServiceProps) => {
   return (
     <div
       className="p-5 sm:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg
-                    shadow-box-shadow relative overflow-hidden"
+                    shadow-box-shadow relative overflow-hidden text-center"
     >
-      <div className="rounded-xl bg-body p-3 text-heading-1 w-max relative">
-        {" "}
-        {icon}{" "}
+      <div className="rounded-xl bg-body p-3 text-heading-1 w-max mx-auto relative">
+        {icon}
       </div>
       <div className="mt-6 space-y-4 relative">
         <h2 className="text-lg md:text-xl font-semibold text-heading-2">
-          {" "}
           {title}
         </h2>
-        <Paragraph> {description}</Paragraph>
+        <Paragraph>{description}</Paragraph>
       </div>
     </div>
   );
